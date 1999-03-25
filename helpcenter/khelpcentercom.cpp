bfb52652ace31640168007e82fea80414e91c480
Matthias Elter <m.elter@kde.org>
1999-03-25 18:42:17 +0000

    khelpcentercom.cpp was missing :-)
    svn path=/trunk/kdebase/khelpcenter/; revision=18611

---
+++ helpcenter/khelpcentercom.cpp
@@ -0,0 +1,106 @@
+/*
+ *  khelpcentercom.cpp - part of the KDE Help Center
+ *
+ *  Copyright (c) 1999 Matthias Elter (me@kde.org)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <komApplication.h>
+#include <kprocess.h>
+
+#include "khelpcenter.h"
+
+void usage()
+{
+  printf(""\
+		 "\nkhelpcentercom is part of the KDE Help Center\n"\
+		 "(c) 1999, Matthias Elter\n\n"\
+		 "purpose: remote control khelpcenter\n\n"\
+		 "syntax:\n"\
+		 "   khelpcentercom open url\n"\
+		 "                 # open a new browser window and load url 'url'\n"\
+		 "                 # load introduction url if no url parameter spezified\n"\
+		 "                 # example url: file:/home/dummy/index.html\n\n"\
+		 "   khelpcentercom configure\n"\
+		 "                 # re-read khelpcenter's configuration\n"\
+		 "options:\n"\
+		 "   -enable_tree\n"\
+		 "                 # open a new browser window with enabled treeview\n");
+}
+
+int main(int argc, char *argv[])
+{
+  bool enableTree = false;
+  int command = 0;
+  QString url;
+  
+  // no parameters -> print usage
+  if(argc == 1) 
+	{
+	  usage();
+	  return 0;
+	}
+
+  // parse command line parameters
+  for (int i = 1; i < argc; i++)
+	{
+	  if (strcasecmp(argv[i], "configure") == 0)
+		{
+		  command = 1;
+		  break;
+		}
+	  else if (strcasecmp(argv[i], "-enable_tree") == 0)
+		{
+		  enableTree = true;
+		}
+	  else if (command == 2) // already found 'open'...this must be a url
+		{
+		  url = argv[i];
+		}
+	  else if (strcasecmp( argv[i], "open" ) == 0)
+		{
+		  command = 2;
+		}
+	}
+
+  if (command == 0)
+	{
+	  usage();
+	  return 0;
+	}
+  
+  KOMApplication app(argc, argv, "khelpcentercom");
+
+  // init CORBA stuff
+  CORBA::Object_var obj = app.orb()->bind("IDL:KHelpCenter/HelpCenterCom:1.0", "inet:localhost:8887");
+  if(CORBA::is_nil(obj))
+	{
+	  printf("It seems that khelpcenter isn't running!\n");
+	  return 0;
+	}	
+  KHelpCenter::HelpCenterCom_var server = KHelpCenter::HelpCenterCom::_narrow(obj);
+  
+  if (command == 1)
+	server->configure();
+  else if (command == 2)
+	{
+	  if (url.isEmpty())
+		url = "file:" + kapp->kde_htmldir() + "/en/khelpcenter/main.html";
+	  server->openHelpView (url, enableTree);
+	}
+  
+  return 0;
+}
