469eb513a913e4ed9608a89f247a5f1639b4d3a6
Waldo Bastian <bastian@kde.org>
2000-08-01 20:04:43 +0000

    WABA: Added command line support
    svn path=/trunk/kdebase/khelpcenter/; revision=59502

--- htmlsearch/index.cpp
+++ htmlsearch/index.cpp
@@ -1,6 +1,8 @@
 #include <qstring.h>
 
 #include <klocale.h>
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
 #include <kglobal.h>
 #include <kapp.h>
 
@@ -9,8 +11,14 @@
 
 int main(int argc, char *argv[])
 {
+  KAboutData aboutData( "khtmlindex", I18N_NOOP("KHtmlIndex"),
+	"$Id:  $",
+	I18N_NOOP("KDE Index generator for help files."));
+
+  KCmdLineArgs::init(argc, argv, &aboutData);
+
   KGlobal::locale()->setMainCatalogue("htmlsearch");
-  KApplication app(argc, argv, "khtmlindex");
+  KApplication app;
   HTMLSearch search;
   
   QString lang="en";
