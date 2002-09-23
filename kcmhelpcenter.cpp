0d0841e3de083432c97b03cf664f3c85864ccb98
Waldo Bastian <bastian@kde.org>
2002-09-23 20:26:44 +0000

    Bring quick help in line with module name
    svn path=/trunk/kdebase/khelpcenter/; revision=179959

--- kcmhelpcenter.cpp
+++ kcmhelpcenter.cpp
@@ -268,7 +268,7 @@ void KCMHelpCenter::slotIndexFinished( KProcess *proc )
 
 QString KCMHelpCenter::quickHelp() const
 {
-  return i18n("<h1>Help Center</h1> With this control module you configure "
+  return i18n("<h1>Help Index</h1> With this control module you configure "
               "and build the index files required by the search function of "
               "the help center.");
 }
