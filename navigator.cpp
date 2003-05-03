e02e0a082621933e68ad7932a61575f0bc7a5116
Frerich Raabe <raabe@kde.org>
2003-05-03 10:33:33 +0000

    - Removed old plugin traverser code which is not needed anymore
    svn path=/trunk/kdebase/khelpcenter/; revision=223596

--- navigator.cpp
+++ navigator.cpp
@@ -64,6 +64,7 @@
 #include "view.h"
 #include "infotree.h"
 #include "mainwindow.h"
+#include "plugintraverser.h"
 
 using namespace KHC;
 
@@ -175,111 +176,6 @@ void Navigator::setupGlossaryTab()
              this, SIGNAL( glossSelected( const GlossaryEntry & ) ) );
     mTabWidget->addTab( mGlossaryTree, i18n( "G&lossary" ) );
 }
-
-class PluginTraverser : public DocEntryTraverser
-{
-  public:
-    PluginTraverser( Navigator *navigator, QListView *listView  ) :
-      mListView( listView ), mParentItem( 0 ), mCurrentItem( 0 ),
-      mNavigator( navigator ) {}
-    PluginTraverser( Navigator *navigator, NavigatorItem *listViewItem  ) :
-      mListView( 0 ), mParentItem( listViewItem ), mCurrentItem( 0 ),
-      mNavigator( navigator ) {}
-
-    void process( DocEntry *entry )
-    {
-      if ( !mListView && !mParentItem ) {
-        kdDebug( 1400 ) << "ERROR! Neither mListView nor mParentItem is set." << endl;
-        return;
-      }
-
-      if ( !entry->docExists() & !mNavigator->showMissingDocs() ) return;
-
-#if 0
-      kdDebug() << "PluginTraverser::process(): " << entry->name()
-                << " (weight: " << entry->weight() << " parent: "
-                << ( mParentItem ? mParentItem->name() : "0" ) << ")" << endl;
-#endif
-
-      if (entry->khelpcenterSpecial() == "apps") {
-        NavigatorAppItem *appItem;
-        if ( mListView )
-          appItem = new NavigatorAppItem( mListView, mCurrentItem );
-        else
-          appItem = new NavigatorAppItem( mParentItem, mCurrentItem );
-        KConfig *cfg = kapp->config();
-        cfg->setGroup( "General" );
-        appItem->setRelpath( cfg->readEntry( "AppsRoot" ) );
-        appItem->setIcon( "kmenu" );
-        mCurrentItem = appItem;
-      } else if ( entry->khelpcenterSpecial() == "scrollkeeper" ) {
-        if ( mParentItem ) {
-          mCurrentItem = mNavigator->insertScrollKeeperDocs( mParentItem,
-                                                             mCurrentItem );
-        }
-        return;
-      } else {
-        if ( mListView )
-          mCurrentItem = new NavigatorItem( mListView, mCurrentItem );
-        else
-          mCurrentItem = new NavigatorItem( mParentItem, mCurrentItem );
-
-        if (entry->khelpcenterSpecial() == "applets" )
-          mNavigator->insertAppletDocs( mCurrentItem );
-
-        else if ( entry->khelpcenterSpecial() == "kinfocenter" ||
-                  entry->khelpcenterSpecial() == "kcontrol" ||
-                  entry->khelpcenterSpecial() == "konqueror" )
-          mNavigator->insertParentAppDocs( entry->khelpcenterSpecial(),
-                                           mCurrentItem );
-
-        else if ( entry->khelpcenterSpecial() == "kioslave" )
-          mNavigator->insertIOSlaveDocs( entry->khelpcenterSpecial(),
-                                         mCurrentItem );
-
-        else if ( entry->khelpcenterSpecial() == "info" ) {
-          mCurrentItem->setPixmap( 0, SmallIcon( "contents2" ) );
-          mNavigator->insertInfoDocs( mCurrentItem );
-        }
-      }
-
-      mCurrentItem->setName( entry->name() );
-      mCurrentItem->setUrl( entry->docPath() );
-
-      if ( mCurrentItem->icon().isNull() ) {
-        if ( !entry->docExists() ) {
-          mCurrentItem->setIcon( "unknown" );
-        } else {
-          if ( entry->icon().isEmpty() ) {
-            if ( entry->isDirectory() ) {
-              mCurrentItem->setIcon( "contents2" );
-            } else {
-              mCurrentItem->setIcon( "document2" );
-            }
-          } else {
-            mCurrentItem->setIcon( entry->icon() );
-          }
-        }
-      }
-    }
-
-    DocEntryTraverser *createChild( DocEntry * )
-    {
-      if ( mCurrentItem ) {
-        return new PluginTraverser( mNavigator, mCurrentItem );
-      }
-      kdDebug( 1400 ) << "ERROR! mCurrentItem is not set." << endl;
-      return 0;
-    }
-
-  private:
-    QListView *mListView;
-    NavigatorItem *mParentItem;
-    NavigatorItem *mCurrentItem;
-    
-    Navigator *mNavigator;
-};
-
 
 void Navigator::insertPlugins()
 {
