273eae8634960967badd21dacbce43bd333628f4
Thiago Macieira <thiago@kde.org>
2005-12-06 04:28:48 +0000

    SVN_SILENT Correcting the encoding in KDE source code
    svn path=/trunk/KDE/kdebase/khelpcenter/; revision=485906

--- htmlsearch/kcmhtmlsearch.h
+++ htmlsearch/kcmhtmlsearch.h
@@ -1,7 +1,7 @@
 /**
  * kcmhtmlsearch.h
  *
- * Copyright (c) 2000 Matthias Hölzer-Klüpfel <hoelzer@kde.org>
+ * Copyright (c) 2000 Matthias HÃ¶lzer-KlÃ¼pfel <hoelzer@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
