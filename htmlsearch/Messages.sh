195f5f45b9beb03176d91c67d6313ee5f5cfdb80
Nicolas Goutte <nicolasg@snafu.de>
2006-02-25 13:01:09 +0000

    Fix typo (Sorry!)
    svn path=/trunk/KDE/kdebase/khelpcenter/; revision=513417

--- htmlsearch/Messages.sh
+++ htmlsearch/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 ### TODO: why do we need 2 POT files for one directory?
-$XGETTEXT index.cpp tmlsearch.cpp progressdialog.cpp rc.cpp -o $podir/htmlsearch.pot
+$XGETTEXT index.cpp htmlsearch.cpp progressdialog.cpp rc.cpp -o $podir/htmlsearch.pot
 $XGETTEXT kcmhtmlsearch.cpp klangcombo.cpp ktagcombobox.cpp rc.cpp -o $podir/kcmhtmlsearch.pot
