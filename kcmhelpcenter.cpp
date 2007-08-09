7d2534f6ec9bfee252718be4cbfb205d2ee1b0bc
Will Stephenson <wstephenson@kde.org>
2007-08-09 16:29:00 +0000

    Winterz says you can't exclude i18n from krazy, and the button won't accept rich text...
    svn path=/trunk/KDE/kdebase/runtime/; revision=698240

--- kcmhelpcenter.cpp
+++ kcmhelpcenter.cpp
@@ -215,7 +215,7 @@ void IndexProgressDialog::toggleDetails()
   if ( mLogView->isHidden() ) {
     mLogLabel->show();
     mLogView->show();
-    mDetailsButton->setText( i18n("Details <<") ); //krazy:exclude-i18ncheckarg
+    mDetailsButton->setText( i18n("Details <<") );
     QSize size = cfg.readEntry( "size", QSize() );
     if ( !size.isEmpty() ) resize( size );
   } else {
@@ -228,7 +228,7 @@ void IndexProgressDialog::hideDetails()
 {
   mLogLabel->hide();
   mLogView->hide();
-  mDetailsButton->setText( i18n("Details >>") ); //krazy:exclude-i18ncheckarg
+  mDetailsButton->setText( i18n("Details >>") );
   layout()->activate();
   adjustSize();
 }
