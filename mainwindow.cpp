b9c03d548712ef5d47eb2b6a898952b86451b516
Michaël Larouche <larouche@kde.org>
2007-10-27 15:07:31 +0000

    Fix compilation with latest kdelibs (use url() instead of baseURL())
    svn path=/trunk/KDE/kdebase/runtime/; revision=729969

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -195,7 +195,7 @@ void MainWindow::enableCopyTextAction()
 void MainWindow::saveProperties( KConfigGroup &config )
 {
     kDebug()<<"void MainWindow::saveProperties( KConfig *config )";
-    config.writePathEntry( "URL" , mDoc->baseURL().url() );
+    config.writePathEntry( "URL" , mDoc->url().url() );
 }
 
 void MainWindow::readProperties( const KConfigGroup &config )
