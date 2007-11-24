32a99a4b2d28bd61516ec2b9b9847b6d2fe4306e
Tom Albers <toma@kde.org>
2007-11-24 19:29:39 +0000

    All contributers agree to a relicense from GPL2 to GPL2/3. Some files had no license or copyright. Corrected that.
    svn path=/trunk/KDE/kdebase/runtime/; revision=741088

--- navigatorappitem.cpp
+++ navigatorappitem.cpp
@@ -3,9 +3,10 @@
  *
  *  Copyright (C) 2001 Waldo Bastian <bastian@kde.org>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License version 2 or at your option version 3 as published
+ *  by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
