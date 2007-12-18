ad1f1a7129f7b8e3fca8375f5a4acbe98c62fd39
Ralf Habacker <ralf.habacker@freenet.de>
2007-12-18 07:21:39 +0000

    untabbing
    svn path=/trunk/KDE/kdebase/runtime/; revision=749891

--- toc.cpp
+++ toc.cpp
@@ -38,236 +38,236 @@ using namespace KHC;
 
 class TOCItem : public NavigatorItem
 {
-	public:
-		TOCItem( TOC *parent, Q3ListViewItem *parentItem, Q3ListViewItem *after, const QString &text );
+    public:
+        TOCItem( TOC *parent, Q3ListViewItem *parentItem, Q3ListViewItem *after, const QString &text );
 
-		const TOC *toc() const { return m_toc; }
+        const TOC *toc() const { return m_toc; }
 
-	private:
-		TOC *m_toc;
+    private:
+        TOC *m_toc;
 };
 
 class TOCChapterItem : public TOCItem
 {
-	public:
-		TOCChapterItem( TOC *toc, NavigatorItem *parent, Q3ListViewItem *after, const QString &title,
-				const QString &name );
+    public:
+        TOCChapterItem( TOC *toc, NavigatorItem *parent, Q3ListViewItem *after, const QString &title,
+                const QString &name );
 
-		virtual QString url();
+        virtual QString url();
 
-	private:
-		QString m_name;
+    private:
+        QString m_name;
 };
 
 class TOCSectionItem : public TOCItem
 {
-	public:
-		TOCSectionItem( TOC *toc, TOCChapterItem *parent, Q3ListViewItem *after, const QString &title,
-				const QString &name );
+    public:
+        TOCSectionItem( TOC *toc, TOCChapterItem *parent, Q3ListViewItem *after, const QString &title,
+                const QString &name );
 
-		virtual QString url();
+        virtual QString url();
 
-	private:
-		QString m_name;
+    private:
+        QString m_name;
 };
 
 TOC::TOC( NavigatorItem *parentItem )
 {
-	m_parentItem = parentItem;
+    m_parentItem = parentItem;
 }
 
 void TOC::build( const QString &file )
 {
-	QFileInfo fileInfo( file );
-	QString fileName = fileInfo.absoluteFilePath();
-	const QStringList resourceDirs = KGlobal::dirs()->resourceDirs( "html" );
-	QStringList::ConstIterator it = resourceDirs.begin();
-	QStringList::ConstIterator end = resourceDirs.end();
-	for ( ; it != end; ++it ) {
-		if ( fileName.startsWith( *it ) ) {
-			fileName.remove( 0, ( *it ).length() );
-			break;
-		}
-	}
+    QFileInfo fileInfo( file );
+    QString fileName = fileInfo.absoluteFilePath();
+    const QStringList resourceDirs = KGlobal::dirs()->resourceDirs( "html" );
+    QStringList::ConstIterator it = resourceDirs.begin();
+    QStringList::ConstIterator end = resourceDirs.end();
+    for ( ; it != end; ++it ) {
+        if ( fileName.startsWith( *it ) ) {
+            fileName.remove( 0, ( *it ).length() );
+            break;
+        }
+    }
 
-	QString cacheFile = fileName.replace( QDir::separator(), "__" );
-	m_cacheFile = KStandardDirs::locateLocal( "cache", "help/" + cacheFile );
-	m_sourceFile = file;
+    QString cacheFile = fileName.replace( QDir::separator(), "__" );
+    m_cacheFile = KStandardDirs::locateLocal( "cache", "help/" + cacheFile );
+    m_sourceFile = file;
 
-	if ( cacheStatus() == NeedRebuild )
-		buildCache();
-	else
-		fillTree();
+    if ( cacheStatus() == NeedRebuild )
+        buildCache();
+    else
+        fillTree();
 }
 
 TOC::CacheStatus TOC::cacheStatus() const
 {
-	if ( !QFile::exists( m_cacheFile ) ||
-	     sourceFileCTime() != cachedCTime() )
-		return NeedRebuild;
+    if ( !QFile::exists( m_cacheFile ) ||
+         sourceFileCTime() != cachedCTime() )
+        return NeedRebuild;
 
-	return CacheOk;
+    return CacheOk;
 }
 
 int TOC::sourceFileCTime() const
 {
-	struct stat stat_buf;
-	stat( QFile::encodeName( m_sourceFile ).data(), &stat_buf );
+    struct stat stat_buf;
+    stat( QFile::encodeName( m_sourceFile ).data(), &stat_buf );
 
-	return stat_buf.st_ctime;
+    return stat_buf.st_ctime;
 }
 
 int TOC::cachedCTime() const
 {
-	QFile f( m_cacheFile );
-	if ( !f.open( QIODevice::ReadOnly ) )
-		return 0;
+    QFile f( m_cacheFile );
+    if ( !f.open( QIODevice::ReadOnly ) )
+        return 0;
 
-	QDomDocument doc;
-	if ( !doc.setContent( &f ) )
-		return 0;
+    QDomDocument doc;
+    if ( !doc.setContent( &f ) )
+        return 0;
 
-	QDomComment timestamp = doc.documentElement().lastChild().toComment();
+    QDomComment timestamp = doc.documentElement().lastChild().toComment();
 
-	return timestamp.data().trimmed().toInt();
+    return timestamp.data().trimmed().toInt();
 }
 
 void TOC::buildCache()
 {
-	K3Process *meinproc = new K3Process;
-	connect( meinproc, SIGNAL( processExited( K3Process * ) ),
-	         this, SLOT( meinprocExited( K3Process * ) ) );
+    K3Process *meinproc = new K3Process;
+    connect( meinproc, SIGNAL( processExited( K3Process * ) ),
+             this, SLOT( meinprocExited( K3Process * ) ) );
 
-	*meinproc << KStandardDirs::locate( "exe", "meinproc4" );
-	*meinproc << "--stylesheet" << KStandardDirs::locate( "data", "khelpcenter/table-of-contents.xslt" );
-	*meinproc << "--output" << m_cacheFile;
-	*meinproc << m_sourceFile;
+    *meinproc << KStandardDirs::locate( "exe", "meinproc4" );
+    *meinproc << "--stylesheet" << KStandardDirs::locate( "data", "khelpcenter/table-of-contents.xslt" );
+    *meinproc << "--output" << m_cacheFile;
+    *meinproc << m_sourceFile;
 
-	meinproc->start( K3Process::NotifyOnExit );
+    meinproc->start( K3Process::NotifyOnExit );
 }
 
 void TOC::meinprocExited( K3Process *meinproc )
 {
-	if ( !meinproc->normalExit() || meinproc->exitStatus() != 0 ) {
-		delete meinproc;
-		return;
-	}
+    if ( !meinproc->normalExit() || meinproc->exitStatus() != 0 ) {
+        delete meinproc;
+        return;
+    }
 
-	delete meinproc;
+    delete meinproc;
 
-	QFile f( m_cacheFile );
-	if ( !f.open( QIODevice::ReadWrite ) )
-		return;
+    QFile f( m_cacheFile );
+    if ( !f.open( QIODevice::ReadWrite ) )
+        return;
 
-	QDomDocument doc;
-	if ( !doc.setContent( &f ) )
-		return;
+    QDomDocument doc;
+    if ( !doc.setContent( &f ) )
+        return;
 
-	QDomComment timestamp = doc.createComment( QString::number( sourceFileCTime() ) );
-	doc.documentElement().appendChild( timestamp );
+    QDomComment timestamp = doc.createComment( QString::number( sourceFileCTime() ) );
+    doc.documentElement().appendChild( timestamp );
 
-	f.seek( 0 );
-	QTextStream stream( &f );
-	stream.setCodec( "UTF-8" );
-	stream << doc.toString();
+    f.seek( 0 );
+    QTextStream stream( &f );
+    stream.setCodec( "UTF-8" );
+    stream << doc.toString();
 
-	f.close();
+    f.close();
 
-	fillTree();
+    fillTree();
 }
 
 void TOC::fillTree()
 {
-	QFile f( m_cacheFile );
-	if ( !f.open( QIODevice::ReadOnly ) )
-		return;
+    QFile f( m_cacheFile );
+    if ( !f.open( QIODevice::ReadOnly ) )
+        return;
 
-	QDomDocument doc;
-	if ( !doc.setContent( &f ) )
-		return;
+    QDomDocument doc;
+    if ( !doc.setContent( &f ) )
+        return;
 
-	TOCChapterItem *chapItem = 0;
-	QDomNodeList chapters = doc.documentElement().elementsByTagName( "chapter" );
-	for ( int chapterCount = 0; chapterCount < chapters.count(); chapterCount++ ) {
-		QDomElement chapElem = chapters.item( chapterCount ).toElement();
-		QDomElement chapTitleElem = childElement( chapElem, QLatin1String( "title" ) );
-		QString chapTitle = chapTitleElem.text().simplified();
-		QDomElement chapRefElem = childElement( chapElem, QLatin1String( "anchor" ) );
-		QString chapRef = chapRefElem.text().trimmed();
+    TOCChapterItem *chapItem = 0;
+    QDomNodeList chapters = doc.documentElement().elementsByTagName( "chapter" );
+    for ( int chapterCount = 0; chapterCount < chapters.count(); chapterCount++ ) {
+        QDomElement chapElem = chapters.item( chapterCount ).toElement();
+        QDomElement chapTitleElem = childElement( chapElem, QLatin1String( "title" ) );
+        QString chapTitle = chapTitleElem.text().simplified();
+        QDomElement chapRefElem = childElement( chapElem, QLatin1String( "anchor" ) );
+        QString chapRef = chapRefElem.text().trimmed();
 
-		chapItem = new TOCChapterItem( this, m_parentItem, chapItem, chapTitle, chapRef );
+        chapItem = new TOCChapterItem( this, m_parentItem, chapItem, chapTitle, chapRef );
 
-		TOCSectionItem *sectItem = 0;
-		QDomNodeList sections = chapElem.elementsByTagName( "section" );
-		for ( int sectCount = 0; sectCount < sections.count(); sectCount++ ) {
-			QDomElement sectElem = sections.item( sectCount ).toElement();
-			QDomElement sectTitleElem = childElement( sectElem, QLatin1String( "title" ) );
-			QString sectTitle = sectTitleElem.text().simplified();
-			QDomElement sectRefElem = childElement( sectElem, QLatin1String( "anchor" ) );
-			QString sectRef = sectRefElem.text().trimmed();
+        TOCSectionItem *sectItem = 0;
+        QDomNodeList sections = chapElem.elementsByTagName( "section" );
+        for ( int sectCount = 0; sectCount < sections.count(); sectCount++ ) {
+            QDomElement sectElem = sections.item( sectCount ).toElement();
+            QDomElement sectTitleElem = childElement( sectElem, QLatin1String( "title" ) );
+            QString sectTitle = sectTitleElem.text().simplified();
+            QDomElement sectRefElem = childElement( sectElem, QLatin1String( "anchor" ) );
+            QString sectRef = sectRefElem.text().trimmed();
 
-			sectItem = new TOCSectionItem( this, chapItem, sectItem, sectTitle, sectRef );
-		}
-	}
+            sectItem = new TOCSectionItem( this, chapItem, sectItem, sectTitle, sectRef );
+        }
+    }
 
   m_parentItem->setOpen( true );
 }
 
 QDomElement TOC::childElement( const QDomElement &element, const QString &name )
 {
-	QDomElement e;
-	for ( e = element.firstChild().toElement(); !e.isNull(); e = e.nextSibling().toElement() )
-		if ( e.tagName() == name )
-			break;
-	return e;
+    QDomElement e;
+    for ( e = element.firstChild().toElement(); !e.isNull(); e = e.nextSibling().toElement() )
+        if ( e.tagName() == name )
+            break;
+    return e;
 }
 
 void TOC::slotItemSelected( Q3ListViewItem *item )
 {
-	TOCItem *tocItem;
-	if ( ( tocItem = dynamic_cast<TOCItem *>( item ) ) )
-		emit itemSelected( tocItem->entry()->url() );
+    TOCItem *tocItem;
+    if ( ( tocItem = dynamic_cast<TOCItem *>( item ) ) )
+        emit itemSelected( tocItem->entry()->url() );
 
-	item->setOpen( !item->isOpen() );
+    item->setOpen( !item->isOpen() );
 }
 
 TOCItem::TOCItem( TOC *toc, Q3ListViewItem *parentItem, Q3ListViewItem *after, const QString &text )
-	: NavigatorItem( new DocEntry( text ), parentItem, after )
+    : NavigatorItem( new DocEntry( text ), parentItem, after )
 {
         setAutoDeleteDocEntry( true );
-	m_toc = toc;
+    m_toc = toc;
 }
 
 TOCChapterItem::TOCChapterItem( TOC *toc, NavigatorItem *parent, Q3ListViewItem *after,
-				const QString &title, const QString &name )
-	: TOCItem( toc, parent, after, title ),
-	m_name( name )
+                const QString &title, const QString &name )
+    : TOCItem( toc, parent, after, title ),
+    m_name( name )
 {
-	setOpen( false );
-	entry()->setUrl(url());
+    setOpen( false );
+    entry()->setUrl(url());
 }
 
 QString TOCChapterItem::url()
 {
-	return QLatin1String("help:") + toc()->application() + QLatin1Char('/') + m_name 
+    return QLatin1String("help:") + toc()->application() + QLatin1Char('/') + m_name 
                + QLatin1String(".html");
 }
 
 TOCSectionItem::TOCSectionItem( TOC *toc, TOCChapterItem *parent, Q3ListViewItem *after,
-				const QString &title, const QString &name )
-	: TOCItem( toc, parent, after, title ),
-	m_name( name )
+                const QString &title, const QString &name )
+    : TOCItem( toc, parent, after, title ),
+    m_name( name )
 {
-	setPixmap( 0, SmallIcon( "document" ) );
-	entry()->setUrl(url());
+    setPixmap( 0, SmallIcon( "document" ) );
+    entry()->setUrl(url());
 }
 
 QString TOCSectionItem::url()
 {
-	if ( static_cast<TOCSectionItem *>( parent()->firstChild() ) == this )
-		return static_cast<TOCChapterItem *>( parent() )->url() + '#' + m_name;
+    if ( static_cast<TOCSectionItem *>( parent()->firstChild() ) == this )
+        return static_cast<TOCChapterItem *>( parent() )->url() + '#' + m_name;
 
-	return "help:" + toc()->application() + '/' + m_name + ".html";
+    return "help:" + toc()->application() + '/' + m_name + ".html";
 }
 
 #include "toc.moc"
