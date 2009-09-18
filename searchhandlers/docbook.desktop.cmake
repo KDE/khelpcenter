3c2bc1d7f4820fdb4ddb5996482e8d5eb6cafba8
David Faure <faure@kde.org>
2009-09-18 00:35:46 +0000

    Also look in /opt/www/htdig/bin  (for Slackware, according to comment #5) CCBUG: 122437
    svn path=/trunk/KDE/kdebase/runtime/; revision=1025099

--- searchhandlers/docbook.desktop.cmake
+++ searchhandlers/docbook.desktop.cmake
@@ -2,7 +2,7 @@
 DocumentTypes=application/docbook+xml
 
 SearchBinary=htsearch
-SearchBinaryPaths=/srv/www/cgi-bin,/usr/lib/cgi-bin
+SearchBinaryPaths=/srv/www/cgi-bin,/usr/lib/cgi-bin,/opt/www/htdig/bin
 SearchCommand=${LIBEXEC_INSTALL_DIR}/khc_htsearch.pl --binary=%b --docbook --indexdir=%d --config=%i --words=%w --method=%o --maxnum=%m --lang=en
 IndexCommand=${LIBEXEC_INSTALL_DIR}/khc_docbookdig.pl --indexdir=%d --docpath=%p --identifier=%i
 TryExec=/usr/bin/htdig
--- searchhandlers/htdig.desktop.cmake
+++ searchhandlers/htdig.desktop.cmake
@@ -2,7 +2,7 @@
 DocumentTypes=text/html
 
 SearchBinary=htsearch
-SearchBinaryPaths=/srv/www/cgi-bin,/usr/lib/cgi-bin
+SearchBinaryPaths=/srv/www/cgi-bin,/usr/lib/cgi-bin,/opt/www/htdig/bin
 SearchCommand=${LIBEXEC_INSTALL_DIR}/khc_htsearch.pl --binary=%b --indexdir=%d --config=%i --words=%w --method=%o --maxnum=%m --lang=%l
 IndexCommand=${LIBEXEC_INSTALL_DIR}/khc_htdig.pl --indexdir=%d --docpath=%p --identifier=%i --lang=%l
 TryExec=/usr/bin/htdig
