efd3ba4b22e1f46b2ed6911ff425b11f42965258
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:44:03 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdebase/runtime/; revision=1166696

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.kcfg"` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name "*.kcfg" -o -name "*.rc"` >> rc.cpp || exit 11
 $XGETTEXT *.cpp -o $podir/khelpcenter.pot
 rm -f rc.cpp
