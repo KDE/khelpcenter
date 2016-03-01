1f231c623f98a7d36b290327f8bfeef8ff39c0bc
Pino Toscano <pino@kde.org>
2016-03-01 08:28:42 +0100

    i18n: stop extracting messages for htmlsearch
    The whole content of htmlsearch is not built at all, and totally
    deprecated.  While it is going to be replaced soon with a better
    solution, stop wasting translators' time with unused messages.

--- htmlsearch/Messages.sh
+++ htmlsearch/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+exit 0  # htmlsearch is not built and deprecated
 ### TODO: why do we need 2 POT files for one directory?
 $XGETTEXT index.cpp htmlsearch.cpp progressdialog.cpp rc.cpp -o $podir/htmlsearch.pot
 $XGETTEXT kcmhtmlsearch.cpp klangcombo.cpp ktagcombobox.cpp rc.cpp -o $podir/kcmhtmlsearch.pot
